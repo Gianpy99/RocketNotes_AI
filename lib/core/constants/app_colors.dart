8c6f494f1464aea706e7d937af97e8396a8fc305
Gianpaolo Borrello <124720503+Gianpy99@users.noreply.github.com>
2025-08-18 14:44:24 +0200

    Update app_colors.dart

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,1 +1,12 @@
+// lib/core/constants/app_colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBlue = Color(0xFF2196F3);
+  static const Color workBlue = Color(0xFF2196F3);
+  static const Color personalGreen = Color(0xFF4CAF50);
+  static const Color accentOrange = Color(0xFFFF5722);
+  static const Color backgroundLight = Color(0xFFFAFAFA);
+  static const Color backgroundDark = Color(0xFF121212);
+}
 
