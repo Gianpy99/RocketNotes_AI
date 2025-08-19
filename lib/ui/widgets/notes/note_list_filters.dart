6327de0b931f4c2ba6a9dc591191194ad38e8eae
Gianpaolo Borrello <124720503+Gianpy99@users.noreply.github.com>
2025-08-19 07:18:54 +0200

    Create note_list_filters.dart

---
+++ lib/ui/widgets/notes/note_list_filters.dart
@@ -0,0 +1,45 @@
+// lib/ui/widgets/notes/note_list_filters.dart
+import 'package:flutter/material.dart';
+import '../../../core/constants/app_colors.dart';
+import '../note_list_screen.dart';
+
+class NoteListFilters extends StatelessWidget {
+  final Set<String> selectedTags;
+  final Function(Set<String>) onTagsChanged;
+  final NoteSortBy sortBy;
+  final bool sortAscending;
+  final Function(NoteSortBy, bool) onSortChanged;
+
+  const NoteListFilters({
+    super.key,
+    required this.selectedTags,
+    required this.onTagsChanged,
+    required this.sortBy,
+    required this.sortAscending,
+    required this.onSortChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Filters',
+            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 12),
+          // TODO: Add tag filter chips and sort options
+          Text(
+            'Tag filters and sort options will be implemented here',
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+        ],
+      ),
+    );
+  }
+}
